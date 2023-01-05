d57a7880fa49897ab331c4a680a28c0adc459b88
kini12 <63780211+kini12@users.noreply.github.com>
2023-01-05 20:46:48 +0100

    update of the script

--- wd.py
+++ wd.py
@@ -10,17 +10,10 @@ The Data class is about getting the data from the fields into the mailmerge func
 coded by kiki @VCH
 
 """
-from mailmerge import *
-
+from mailmerge import MailMerge
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
-
-#import win32com.client as win32
-
-from pathlib import Path
-import shutil
-import datetime
 import os
 
 # class 
@@ -31,47 +24,41 @@ class data():
         os.system("explorer output")
 
     def get_data():
-        global name
-        global first_name
-        global age
-        global location
-        global mail
+        global name_form
+        global first_name_form
+        global age_form
+        global location_form
+        global mail_form
 
-        name=name.get()
-        first_name = first_name.get()
-        age = age.get()
-        location = location.get()
-        mail = mail.get()
+        name_form=name_p.get()
+        first_name_form = first_name_p.get()
+        age_form = age_p.get()
+        location_form = location_p.get()
 
-    def send_data():
-        print(name, first_name, age, location, mail)
-        return (name, first_name, age, location, mail)
+        #print(name_form, first_name_form, age_form, location_form, mail_form)
+        return (name_form, first_name_form, age_form, location_form)
+
 
     def publish():
         cwd = os.getcwd()
+        print(cwd)
         path = cwd + r"\output"
+        print(path)
         if os.path.isdir(path) and os.path.exists(path):
-            if len(os.listdir(path)) == 0:
-                data.get_data()
-                print("data copied into the word file")
-
-
-                template_1 = "temp/file.docx"
-                document_1 = MailMerge(template_1)
-                document_1.merge(name_p = name, first_n_p = first_name, age_p = age, location_p = location)
-                document_1.write('file_output.docx')
-
-            else:
-                print("Output not empty. Erase file in it and try again.")
-                messagebox.showinfo("Warning", "Output folder not empty")
+            data.get_data()
+            print("data is about to be copied into the word file")
+            template_1 = "temp/file_1.docx"
+            document_1 = MailMerge(template_1)
+            document_1.merge(name_w = name_form, first_n_w = first_name_form, age_w = age_form, location_w = location_form)
+            document_1.write('output/file_output.docx')
+            os.rename('output/file_output.docx', "output/%s_%s.docx" %(first_name_form, name_form))
+            print("data copied") 
         else:
-            print("Output folder not present anymore")
-            messagebox.showinfo("Warning", "Folder not present anymore")
+            print("Output not empty. Erase file in it and try again.")
+            messagebox.showinfo("Warning", "Output folder not empty")
+
   
-                
-
-
-
+            
 # class for the front office aka the visual app 
 class app(tk.Frame):
     def __init__(self):
@@ -80,11 +67,23 @@ class app(tk.Frame):
 if __name__ == "__main__":
     window=tk.Tk()
     window.title("Word Deployer")
-    window.geometry("500x280")
+    window.geometry("350x200")
     window.resizable(width=True, height=True)
     # 1 these global variables are how we can have a value in the field, and then still use the user input to be what's going to be mailmerged
 
 
+    global name_p
+    name = tk.StringVar(window)
+    name.set(name)
+    global first_name_p
+    first_name = tk.StringVar(window)
+    first_name.set(first_name)
+    global age_p
+    age = tk.StringVar(window)
+    age.set(age)
+    global location_p
+    location = tk.StringVar(window)
+    location.set(location)
 
     #Labels
     l1=Label(window, text= "Name")
@@ -93,33 +92,27 @@ if __name__ == "__main__":
     l2.grid(row=2,column=0, pady=2)
     l3=Label(window, text= "Age")
     l3.grid(row=4,column=0, pady=2)
-    l4=Label(window, text= "E-Mail")
-    l4.grid(row=6,column=0, pady=2)
-    l5=Label(window, text= "Location")
-    l5.grid(row=8,column=0, pady=4)
+    l4=Label(window, text= "Location")
+    l4.grid(row=6,column=0, pady=4)
+
+
 
     #Entry fields
-    name=StringVar()
-    e1=Entry(window, width=40, bd=1, textvariable=name)
+    name_p=StringVar()
+    e1=Entry(window, width=40, bd=1, textvariable=name_p)
     e1.grid(row=0, column=1, pady=2)
-    first_name=StringVar()
-    e2=Entry(window, width=40, bd=1, textvariable=first_name)
+    first_name_p=StringVar()
+    e2=Entry(window, width=40, bd=1, textvariable=first_name_p)
     e2.grid(row=2, column=1, pady=2)
-    age=StringVar()
-    e3=Entry(window, width=40, bd=1, textvariable=age)
+    age_p=StringVar()
+    e3=Entry(window, width=40, bd=1, textvariable=age_p)
     e3.grid(row=4, column=1, pady=2)
-    mail=StringVar()
-    e4=Entry(window, width=40, bd=1, textvariable=mail)
+    location_p=StringVar()
+    e4=Entry(window, width=40, bd=1, textvariable=location_p)
     e4.grid(row=6, column=1, pady=2)
-    location=StringVar()
-    e5=Entry(window, width=40, bd=1, textvariable=location)
-    e5.grid(row=8, column=1, pady=2)
-    
-
-
     
     #Buttons
-    b1=Button(window, text="Publish", width=16, borderwidth=1, relief="raised", activebackground="green", overrelief="sunken", command = data.get_data)
+    b1=Button(window, text="Publish", width=16, borderwidth=1, relief="raised", activebackground="green", overrelief="sunken", command = data.publish)
     b1.grid(row=11, column=1, pady=2)
     b2=Button(window,text="Output", width=16, borderwidth=1, relief="raised", fg="blue", activebackground="green", overrelief="sunken", command=data.open)
     b2.grid(row=12, column=1, pady=2)
